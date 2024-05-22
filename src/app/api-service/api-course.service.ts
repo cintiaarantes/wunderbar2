2b94314b96eb85eb02dffc9449ee446370c14233
DeividsonOmedio <deividsonomedio@outlook.com>
2024-05-21 21:43:18 -0300

    atualizado

--- src/app/api-service/api-course.service.ts
+++ src/app/api-service/api-course.service.ts
@@ -9,7 +9,7 @@ import { BehaviorSubject } from 'rxjs';
 export class ApiCourseService {
   private coursersSubject = new BehaviorSubject<ICourse[]>([]);
   coursersList = this.coursersSubject.asObservable();
-  API_Url = 'https://crudcrud.com/api/0740b4508270448d9fd93fd8259005c1/courses';
+  API_Url = 'https://crudcrud.com/api/726f74e87d2b45fcb701aee0285e4c39/courses';
   
   constructor(private http: HttpClient) {
     this.getAllCourses();
@@ -29,7 +29,7 @@ export class ApiCourseService {
   }
 
   addCourse(course: ICourse) {
-    return this.http.post(this.API_Url, course);
+    return this.http.post(this.API_Url, course);  
   }
 
   updateCourse(course: ICourse) {
@@ -72,12 +72,18 @@ export class ApiCourseService {
         img: 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/2%20(1)_Kp6XWwq.jpg',
         price: 'USD 50'
       },
-  
       {
         id: 5,
         name: 'React Native',
         level: 'Advanced',
         img: 'https://logos-world.net/wp-content/uploads/2021/08/Android-Logo-2017-2019.png',
+        price: 'USD 55'
+      },
+      {
+        id: 6,
+        name: 'Node Js',
+        level: 'Advanced',
+        img: 'https://th.bing.com/th/id/OIP.CTvVfHEeovAx-0V5-dqVRwHaHa?w=163&h=180&c=7&r=0&o=5&pid=1.7',
         price: 'USD 55'
       }
     ];
